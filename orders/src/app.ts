6f17baf2eaa10ba5772830a1b0518ff16faea444
Aman Vishwakarma <amanvish.vns@gmail.com>
2024-09-09 01:22:56 +0530

    feat: orders module

---
+++ orders/src/app.ts
@@ -0,0 +1,33 @@
+import { NotFoundError, currentUser, errorHandler } from '@aman-tickets/common';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+import 'express-async-errors';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
